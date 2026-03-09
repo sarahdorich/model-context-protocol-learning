ce2bda3a55399215f2ac2d14406792a524531a6e
statssarah1 <sarah.dorich@locumsmart.net>
2026-03-09 10:00:09 -0600

    Initial setup

---
+++ mcp_server.py
@@ -0,0 +1,27 @@
+# The mcp SDK provides a simple interface for building MCPs. 
+# IT also makes it easy to create tools.
+from mcp.server.fastmcp import FastMCP
+
+# This creates a simple MCP with the name "DocumentMCP" and sets the log level to "ERROR".
+mcp = FastMCP("DocumentMCP", log_level="ERROR")
+
+
+docs = {
+    "deposition.md": "This deposition covers the testimony of Angela Smith, P.E.",
+    "report.pdf": "The report details the state of a 20m condenser tower.",
+    "financials.docx": "These financials outline the project's budget and expenditures.",
+    "outlook.pdf": "This document presents the projected future performance of the system.",
+    "plan.md": "The plan outlines the steps for the project's implementation.",
+    "spec.txt": "These specifications define the technical requirements for the equipment.",
+}
+
+# TODO: Write a tool to read a doc
+# TODO: Write a tool to edit a doc
+# TODO: Write a resource to return all doc id's
+# TODO: Write a resource to return the contents of a particular doc
+# TODO: Write a prompt to rewrite a doc in markdown format
+# TODO: Write a prompt to summarize a doc
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
